f93bf9316981c05804d84dfebc37acbfe19d5815
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-22 13:09:28 +0530

    shared library

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -1,42 +1,45 @@
 def call() {
-    node('node1') {
+    node('ci-server') {
+
         stage('CodeCheckout') {
+
             sh "find ."
-            sh "find . | sed -e '1d' | xargs rm -rf"
-            if (env.TAG_NAME ==~ '.*') {
+            sh "find . | sed -e '1d' |xargs rm -rf"
+            if(env.TAG_NAME ==~ ".*") {
                 env.branch_name = "refs/tags/${env.TAG_NAME}"
             } else {
                 env.branch_name = "${env.BRANCH_NAME}"
             }
             checkout([$class: 'GitSCM',
-                    branches: [[name: "${branch_name}"]],
-                    userRemoteConfigs: [[url: "https://github.com/devps23/expense-${component}"]]]
+                      branches: [[name: "${branch_name}"]],
+                      userRemoteConfigs: [[url: "https://github.com/devps23/expense-${component}"]]]
             )
         }
-    }
-    if (env.TAG_NAME ==~ '.*') {
-        stage('Build Code') {
-            sh "env"
-            print 'OK'
+
+        if (env.TAG_NAME ==~ '.*') {
+            stage('Build Code') {
+                sh 'docker build -t 041445559784.dkr.ecr.us-east-1.amazonaws.com/expense-backend:${TAG_NAME} .'
+            }
+            stage('Release Software') {
+               print 'OK'
+            }
+                   } else {
+            stage('Lint Code') {
+                print 'OK'
+            }
+            if(env.BRANCH_NAME != 'main') {
+                stage('Run Unit tests') {
+                    print 'OK'
+                }
+                stage('Run Integration tests') {
+                    print 'OK'
+                }
+            }
+            stage('Sonar Scan Code Review') {
+                print 'OK'
+            }
+
         }
-        stage('Release Software') {
-            print 'OK'
-        }
-    } else {
-        stage('Lint code') {
-            print 'OK'
-        }
-    }
-    if (env.BRANCH_NAME != 'main') {
-        stage('Run unit tests') {
-            print 'OK'
-        }
-        stage('Run integration tests') {
-            print 'OK'
-        }
-    } else {
-        stage('Sonar Scan Code Review') {
-            print 'OK'
-        }
+
     }
 }
