7bcbcc7ea099cbdcf911d32a6a003d5f0e5543d4
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-22 17:11:03 +0530

    create a Dockerfile

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -21,6 +21,8 @@ def call() {
                 sh 'docker build -t 041445559784.dkr.ecr.us-east-1.amazonaws.com/expense-backend:${TAG_NAME} .'
             }
             stage('Release Software') {
+                sh 'docker login aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 041445559784.dkr.ecr.us-east-1.amazonaws.com'
+                sh 'docker push 041445559784.dkr.ecr.us-east-1.amazonaws.com/expense-backend:${TAG_NAME}'
                print 'OK'
             }
                    } else {
