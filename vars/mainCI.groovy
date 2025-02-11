69fbc1dd39f8d754ddef8836eb26604934b81825
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-11 11:44:52 +0530

    jenkins multi branch

--- vars/mainCI.groovy
+++ vars/mainCI.groovy
@@ -19,7 +19,6 @@ def call() {
         if (env.TAG_NAME ==~ '.*') {
             stage('Build Code') {
                 sh 'docker build -t 041445559784.dkr.ecr.us-east-1.amazonaws.com/expense-${component}:${TAG_NAME} .'
-
             }
             stage('Release Software') {
                 sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 041445559784.dkr.ecr.us-east-1.amazonaws.com'
